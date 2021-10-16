28bf78b3d9d2427452a89f3ccbb1b7a1134aff56
kenny-tse <kennytse91@gmail.com>
2021-10-16 23:15:22 +0000

    rendering index template

--- routes/index.js
+++ routes/index.js
@@ -15,10 +15,8 @@ module.exports = (db) => {
   router.get("/", (req, res) => {
 
     //need to see if there is user_id key in cookie
-    //const templateVars = {userId here incase they are at index and logged in};
-    // res.render("ejs template name",templateVars);
-
-    res.send("GET to /index");
+    const templateVars = { hello: "userId here incase they are at index and logged in" };
+    res.render("index", templateVars);
   });
   return router;
 };
