e7ba6de8634ac898b0bcafe338f25f27a0f0348a
kenny-tse <kennytse91@gmail.com>
2021-10-17 23:03:29 +0000

    adjusted explore.js code for dynamic rendering

--- routes/explore.js
+++ routes/explore.js
@@ -3,16 +3,15 @@ const router = express.Router();
 
 module.exports = (db) => {
 
-  //this page will be initially populated by this get
+  //when you enter the explore page, sends map data to client
   router.get("/", (req, res) => {
 
     // functionToQuerySomeMapsToDisplayFromDatabase()
-    // pass the templateVars to render so ejs can fill the page with a list of maps
+    // pass the output to client side so client side can render
 
     // db.functionToQuerySomeMapsToDisplayFromDatabase()
     //   .then((allMapsWeGot) => {
-    //     const templateVars = allMapsWeGot;
-    //     res.render("name_chosen_for_explore_template.ejs", templateVars);
+    //     res.send({allMapsWeGot});
     //   })
     //   .catch(e => {
     //     console.error(e);
@@ -22,23 +21,23 @@ module.exports = (db) => {
     res.send("GET to /explore")
   });
 
-  //page should dynamically load most popular (not reload)
+  //when most popular button is pressed, sends popular maps obj to client
   router.get("/most_popular", (req, res) => {
 
-    // //function to return query to grabs maps with many visits? high rating?
-    // const templateVars = db.popularMaps();
+    // db.popularMaps()
+    //   .then((popularMapsWeGot) => {
+    //      res.send({popularMapsWeGot});
+    //   })
+    //   .catch(e => {
+    //     console.error(e);
+    //     res.send(e)
+    //   });
 
-    // //pass the templateVars to render so ejs can fill
-    // //the page with a list of maps
-    // res.render("name_chosen_for_explore_template.ejs", templateVariable);
-
-    // since it will by DynamicsCompressorNode, there will be no res.render?
     res.send("GET to /most_popular");
   });
 
-  //when someone clicks a map image or title or looking at a single map
+  // when you click on a map to look at, renders localhost:8080/explore/:id
   router.get("/:id", (req, res) => {
-
     // button or hyperlink will need to supply the map id they clicked on
     // const mapIdToSearch = res.body.mapId;
     // database function to return map data given its id from the form (res.body.mapId) to pass to render
