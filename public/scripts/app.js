2ed6a0753e110b3534b0190e1d17e18da45c90d5
Colin Poon <me@colinpoon.com>
2021-10-16 21:24:54 -0700

    index search bar applied

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -7,8 +7,13 @@ const escape = function (str) {
 };
 
 $(document).ready(function () {
+const clearInput = () => {
+  const input = document.getElementsByTagName("input")[0];
+  input.value = "";
+}
 
-
+const clearBtn = document.getElementById("clear-btn");
+clearBtn.addEventListener("click", clearInput);
 
 
 
