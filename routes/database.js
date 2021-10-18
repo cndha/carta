1b4a9b15c5110fce74d4ffca4cba92ab2e92c0f2
Cindy H <cindyhaa@gmail.com>
2021-10-18 18:06:15 +0000

    added getUserById query function

---
+++ routes/database.js
@@ -0,0 +1,24 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'labber',
+  password: 'labber',
+  host: 'localhost',
+  database: 'midterm'
+});
+
+
+const getUserById = function(id) {
+
+  return pool
+  .query(`SELECT * FROM users WHERE id = $1;`, [id])
+  .then(res => {
+    if (res.rows.length === 0) {
+      return null;
+    }
+    return res.rows[0];
+  })
+  .catch(err => console.log(err.message));
+
+}
+exports.getUserById = getUserById;
