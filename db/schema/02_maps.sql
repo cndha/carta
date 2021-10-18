39e7d2cb7805a47ac4747367847479b04b4862d9
Cindy H <cindyhaa@gmail.com>
2021-10-18 16:30:04 +0000

    created data tables

---
+++ db/schema/02_maps.sql
@@ -0,0 +1,22 @@
+DROP TABLE IF EXISTS maps CASCADE;
+
+CREATE TABLE maps (
+  id SERIAL PRIMARY KEY NOT NULL,
+  owned_by INTEGER REFERENCES users(id) ON DELETE CASCADE,
+
+  title VARCHAR(255) NOT NULL,
+  description TEXT,
+  category VARCHAR(225),
+  city VARCHAR(223) NOT NULL,
+  province VARCHAR(223) NOT NULL,
+  country VARCHAR(223) NOT NULL,
+  likes INTEGER,
+  created_at TIMESTAMP,
+  completed_at TIMESTAMP,
+  last_edit TIMESTAMP,
+  saved BOOLEAN DEFAULT false,
+  deleted BOOLEAN DEFAULT false,
+  public BOOLEAN DEFAULT false,
+  private BOOLEAN DEFAULT true,
+);
+
---
+++ db/schema/03_contributors.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS contributors CASCADE;
+
+CREATE TABLE contributors (
+  id SERIAL PRIMARY KEY NOT NULL,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  map_id INTEGER REFERENCES maps(id) ON DELETE CASCADE,
+  contribution_date TIMESTAMP,
+);
