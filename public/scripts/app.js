4b988c01e67838b348c1970b80680ca0f8e03626
Colin Poon <me@colinpoon.com>
2021-10-17 21:53:47 -0700

    mapbox api still wont render

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -7,7 +7,17 @@ const escape = function (str) {
 };
 
 $(document).ready(function () {
-
+  map.on('load', () => {
+    map.addLayer({
+      id: 'terrain-data',
+      type: 'line',
+      source: {
+        type: 'vector',
+        url: 'mapbox://mapbox.mapbox-terrain-v2'
+      },
+      'source-layer': 'contour'
+    });
+  });
 
 
 });
