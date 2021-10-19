64c2b3a9456a0b8172118a0a8d7ae722a012dc9e
Colin Poon <me@colinpoon.com>
2021-10-19 14:40:54 -0700

    dynamically add marker

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -53,4 +53,35 @@ $(document).ready(function () {
       }
     });
   }
+
+  
 });
+// Dynamically add marker to Google Map
+
+// var contactLatitude = 42;
+// var contactLongitude = -72;
+
+// function initialize() {
+//   var mapCanvas = document.getElementById('map');
+//   var myLatLng = {
+//     lat: contactLatitude,
+//     lng: contactLongitude
+//   };
+//   var mapOptions = {
+//     center: new google.maps.LatLng(contactLatitude, contactLongitude),
+//     zoom: 8,
+//     mapTypeId: google.maps.MapTypeId.ROADMAP
+//   }
+//   var map = new google.maps.Map(mapCanvas, mapOptions);
+//   addMarker(myLatLng, map);
+// }
+
+// function addMarker(location, map) {
+//   var marker = new google.maps.Marker({
+//     position: location,
+//     title: 'Home Center',
+//     map: map
+//   });
+// }
+
+// google.maps.event.addDomListener(window, 'load', initialize);
