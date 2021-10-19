d5d8423407b997481e2ca7b4d514b736c7c1782c
Colin Poon <me@colinpoon.com>
2021-10-18 23:03:28 -0700

    YAY got response from search query

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -19,19 +19,11 @@ $(document).ready(function () {
      
     map.addControl(geocoder);
 
- 
-    console.log("geocoder", geocoder.types); 
-    console.log("mapboxgl", mapboxgl); 
-
-
-    console.log(map);
     map.scrollZoom.disable();
   //GET LOCATION DATA ON CLICK
   // map.on("click", (event) => {
   //   console.log(event);
   // })
-
-
   const geojson = {
     type: 'FeatureCollection',
     features: [
