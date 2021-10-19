4fe7ce80cdbffe816dc6f1cc65f3200707b4aa2b
Colin Poon <me@colinpoon.com>
2021-10-18 18:28:02 -0700

    scroll to top

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,67 +3,59 @@ console.log("hello");
 $(document).ready(function () {
   console.log("HELLO");
   mapboxgl.accessToken = 'pk.eyJ1IjoiY29wbzEyMyIsImEiOiJja3V2dTdvdHMxcjdrMm9xanBtaGdkaHc4In0.cBeBLEBv8OE9UnZgr7EEzQ';
-  var map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/satellite-streets-v11',
-    center: [-123.116226, 49.246292],
-    zoom: 11.15
-  });
-  map.on("click", (event) => {
-    console.log(event);
-  })
-  const geocoder = new MapboxGeocoder({
-    accessToken: mapboxgl.accessToken,
-    marker: {
-      color: 'orange'
-    },
-    mapboxgl: mapboxgl
-  });
+          var map = new mapboxgl.Map({
+            container: 'map',
+            style: 'mapbox://styles/mapbox/satellite-streets-v11',
+            center: [-123.116226, 49.246292],
+            zoom: 11.15
+          });
+          map.on("click", (event) => {
+            console.log(event);
+          })
+          map.scrollZoom.disable();
+          map.addControl(new mapboxgl.NavigationControl());
 
-  map.addControl(geocoder);
-  map.addControl(new mapboxgl.NavigationControl());
-
-  const geojson = {
-    type: 'FeatureCollection',
-    features: [
-      {
-        type: 'Feature',
-        geometry: {
-          type: 'Point',
-          coordinates: [-123.1340, 49.2712]
-        },
-        properties: {
-          title: 'yummmmmmy',
-          description: 'Granville Island'
-        }
-      },
-      {
-        type: 'Feature',
-        geometry: {
-          type: 'Point',
-          coordinates: [-123.1683, 49.2684]
-        },
-        properties: {
-          title: 'Kits',
-          description: 'Kitsilano'
-        }
-      }
-    ]
-  };
-  // add markers to map
-  for (const feature of geojson.features) {
-    // create a HTML element for each feature
-    const el = document.createElement('div');
-    el.className = 'marker';
-    // make a marker for each feature and add to the map
-    new mapboxgl.Marker(el).setLngLat(feature.geometry.coordinates).addTo(map)
-      .setPopup(
-        new mapboxgl.Popup({ offset: 25 }) // add popups
-          .setHTML(
-            `<h3 class="pop-title">${feature.properties.title}</h3><p>${feature.properties.description}</p>`
-          )
-      );
-  }
+          const geojson = {
+            type: 'FeatureCollection',
+            features: [
+              {
+                type: 'Feature',
+                geometry: {
+                  type: 'Point',
+                  coordinates: [-123.1340, 49.2712]
+                },
+                properties: {
+                  title: 'yummmmmmy',
+                  description: 'Granville Island'
+                }
+              },
+              {
+                type: 'Feature',
+                geometry: {
+                  type: 'Point',
+                  coordinates: [-123.1683, 49.2684]
+                },
+                properties: {
+                  title: 'Kits',
+                  description: 'Kitsilano'
+                }
+              }
+            ]
+          };
+          // add markers to map
+          for (const feature of geojson.features) {
+            // create a HTML element for each feature
+            const el = document.createElement('div');
+            el.className = 'marker';
+            // make a marker for each feature and add to the map
+            new mapboxgl.Marker(el).setLngLat(feature.geometry.coordinates).addTo(map)
+              .setPopup(
+                new mapboxgl.Popup({ offset: 25 }) // add popups
+                  .setHTML(
+                    `<h3 class="pop-title">${feature.properties.title}</h3><p>${feature.properties.description}</p>`
+                  )
+              );
+          }
 
   // scroll to top        
   $(function () {
