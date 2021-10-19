7fb0c34b32e966a973c9bb8e99bd140ce79d6b6f
Colin Poon <me@colinpoon.com>
2021-10-19 12:36:29 -0700

    geo encoding option on index.ejs

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -18,8 +18,6 @@ $(document).ready(function () {
     });
     marker.addListener("click", toggleBounce);
   }
-  
-  
   function toggleBounce() {
     if (marker.getAnimation() !== null) {
       marker.setAnimation(null);
@@ -28,4 +26,31 @@ $(document).ready(function () {
     }
   }
   initMap();
+
+  var geocoder;
+  var map;
+  function initialize() {
+    geocoder = new google.maps.Geocoder();
+    var latlng = new google.maps.LatLng(-34.397, 150.644);
+    var mapOptions = {
+      zoom: 8,
+      center: latlng
+    }
+    map = new google.maps.Map(document.getElementById('map'), mapOptions);
+  }
+
+  function codeAddress() {
+    var address = document.getElementById('address').value;
+    geocoder.geocode( { 'address': address}, function(results, status) {
+      if (status == 'OK') {
+        map.setCenter(results[0].geometry.location);
+        var marker = new google.maps.Marker({
+            map: map,
+            position: results[0].geometry.location
+        });
+      } else {
+        alert('Geocode was not successful for the following reason: ' + status);
+      }
+    });
+  }
 });
