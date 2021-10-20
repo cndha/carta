35b58d52f1b3820df92ec4ed94bdffb30e328505
Colin Poon <me@colinpoon.com>
2021-10-19 18:30:54 -0700

    add multiple markers

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -3,57 +3,81 @@ $(document).ready(function () {
   console.log("👻");
 
   let marker;
+  // In the following example, markers appear when the user clicks on the map.
+// Each marker is labeled with a single alphabetical character.
+const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let labelIndex = 0;
 
-  function initMap() {
-    const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 12.5,
-      center: { lat: 49.246292, lng: -123.116226 },
-    });
-    map.addListener("click", (event) => {
-      // console.log("LAT--->", event.latLng.lat());
-      $('.latitudeBox').val(event.latLng.lat());
+function initMap() {
+  const vancouver = { lat: 49.246292, lng: -123.116226 };
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 12,
+    center: vancouver,
+  });
 
-      // console.log("LNG--->", event.latLng.lng());
-      $('.longitudeBox').val(event.latLng.lng());
-    })
+  // This event listener calls addMarker() when the map is clicked.
+  google.maps.event.addListener(map, "click", (event) => {
+    addMarker(event.latLng, map);
+  });
+  // Add a marker at the center of the map.
+  addMarker(vancouver, map);
+}
+ 
 
+// Adds a marker to the map.
+function addMarker(location, map) {
+  // Add the marker at the clicked location, and add the next-available label
+  // from the array of alphabetical characters.
+  new google.maps.Marker({
+    position: location,
+    label: labels[labelIndex++ % labels.length],
+    map: map,
+  });
 
-        //  CREATES A NEW CUSTOM TITLED MARKER
-    // function initMap() {
-    //   const myLatLng = { lat: -123.13922862451172, lng: 49.29137637642092 };
-    //   const map = new google.maps.Map(document.getElementById("map"), {
-    //     zoom: 13,
-    //     center: myLatLng,
-    //   });
-    
-    //   new google.maps.Marker({
-    //     position: myLatLng,
-    //     map,
-    //     title: "Hello World!",
-    //   });
-    // }
+  map.addListener("click", (event) => {
+    // console.log("LAT--->", event.latLng.lat());
+    $('.latitudeBox').val(event.latLng.lat());
 
+    // console.log("LNG--->", event.latLng.lng());
+    $('.longitudeBox').val(event.latLng.lng());
+  })
+}
 
+/// ------------> BELOW DOESNT PLACE A MARKER. 
+///  -----------> lng and lat from event listener "onclick"
 
+  // function initMap() {
+  //   const map = new google.maps.Map(document.getElementById("map"), {
+  //     zoom: 12.5,
+  //     center: { lat: 49.246292, lng: -123.116226 },
+  //   });
+  //   map.addListener("click", (event) => {
+  //     // console.log("LAT--->", event.latLng.lat());
+  //     $('.latitudeBox').val(event.latLng.lat());
 
-    marker = new google.maps.Marker({
-      map,
-      draggable: true,
-      animation: google.maps.Animation.DROP,
-      position: { lat: 49.251754722903854, lng: -122.97958354394531 },
+  //     // console.log("LNG--->", event.latLng.lng());
+  //     $('.longitudeBox').val(event.latLng.lng());
+  //   })
 
-      //marker location
-    });
-    marker = new google.maps.Marker({
-      map,
-      draggable: true,
-      animation: google.maps.Animation.DROP,
-      position: { lat: 49.273321738192486, lng: -123.24619670100792 },
-    })
+  //   marker = new google.maps.Marker({
+  //     map,
+  //     draggable: true,
+  //     animation: google.maps.Animation.DROP,
+  //     position: { lat: 49.251754722903854, lng: -122.97958354394531 },
 
-    marker.addListener("click", toggleBounce);
-  }
-  //ajAx request -> db
+  //     //marker location
+  //   });
+  //   marker = new google.maps.Marker({
+  //     map,
+  //     draggable: true,
+  //     animation: google.maps.Animation.DROP,
+  //     position: { lat: 49.273321738192486, lng: -123.24619670100792 },
+  //   })
+
+  //   marker.addListener("click", toggleBounce);
+  // }
+  
+  // //ajAx request -> db
 
   function toggleBounce() {
     if (marker.getAnimation() !== null) {
