f6ecbe4274488077a2af116ca2e36d7f74c5460d
Colin Poon <me@colinpoon.com>
2021-10-20 13:33:57 -0700

    AJAX POST CYCLE

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -8,7 +8,7 @@ $(document).ready(function () {
 
   let marker;
   // Each marker is labeled with a single alphabetical character.
-  const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; 
+  const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   let labelIndex = 0;
 
   function initMap() {
@@ -33,16 +33,18 @@ $(document).ready(function () {
     // });
 
     const contentString = "Food n' Stuff";
+
     const infowindow = new google.maps.InfoWindow({
       content: contentString,
     });
+
     const marker = new google.maps.Marker({
-      position: {lat: 49.2727014,  lng:-123.1352146},
+      position: { lat: 49.2727014, lng: -123.1352146 },
       map,
       title: "Public Market",
       icon: '/IMGS/marker-small.png'
     });
-    
+
     marker.addListener("click", () => {
       infowindow.open({
         anchor: marker,
@@ -67,17 +69,63 @@ $(document).ready(function () {
     // from the array of alphabetical characters.
     new google.maps.Marker({
       position: location,
-      // label: labels[labelIndex++ % labels.length],
+      label: labels[labelIndex++ % labels.length],
       map: map,
     });
 
     map.addListener("click", (event) => {
       // console.log("LAT--->", event.latLng.lat());
-      $('.latitudeBox').val(event.latLng.lat());
-
+      const lat = event.latLng.lat();
       // console.log("LNG--->", event.latLng.lng());
-      $('.longitudeBox').val(event.latLng.lng());
+      const lng = event.latLng.lng();
+
+      const latLng = `${lat}, ${lng}`;
+      axios.get('https://maps.googleapis.com/maps/api/geocode/json', {
+        params: {
+          address: latLng,
+          key: 'AIzaSyCloL_uI_F9x3edJ_zViI7qC5zoq9u2HZg'
+        }
+      })
+        .then(res => {
+          console.log(res);
+          //formatted address:
+          const formattedAddress = res.data.results[0].formatted_address;
+          const outputAddress = `
+                <class="list">
+                  <li id="formatted-address">${formattedAddress}</li>
+                </class>`;
+          //loop through address components
+          const addressComponents = res.data.results[0].address_components;
+          // let componentsOutput = `
+          // <class="list">
+          //   <li>${addressComponents[0].types[0]}: ${addressComponents[0].long_name}</li>
+          //   <li>${addressComponents[1].types[0]}: ${addressComponents[1].long_name}</li>
+          //   <li>${addressComponents[3].types[0]}: ${addressComponents[3].long_name}</li>
+          //   <li>${addressComponents[5].types[0]}: ${addressComponents[5].long_name}</li>
+          //   <li>${addressComponents[6].types[0]}: ${addressComponents[6].long_name}</li>
+          //   <li>${addressComponents[7].types[0]}: ${addressComponents[7].long_name}</li>
+          // </class>`;
+          let componentsOutput = '<class="list">';
+          for (var i = 0; i < addressComponents.length; i++) {
+            componentsOutput += `<li>${addressComponents[i].types[0]}: ${addressComponents[i].long_name}</li>`
+          }
+          componentsOutput += '</class>';
+
+          //lat-long
+          const lat = res.data.results[0].geometry.location.lat;
+          const lng = res.data.results[0].geometry.location.lng;
+          const geometryOutput = `<li>Latitude: ${lat}</li><li>Longitude: ${lng}</li>`;
+
+          //outputs to browser
+          document.getElementById('formatted_address').innerHTML = outputAddress;
+          document.getElementById('components').innerHTML = componentsOutput;
+          document.getElementById('geometry').innerHTML = geometryOutput;
+        })
+        .catch(error => {
+          console.log(error)
+        });
     })
+  //init
   }
 
   // $(function() {
@@ -92,27 +140,16 @@ $(document).ready(function () {
   //   });
   // });
 
-
-
-
-  function toggleBounce() {
-    if (marker.getAnimation() !== null) {
-      marker.setAnimation(null);
-    } else {
-      marker.setAnimation(google.maps.Animation.BOUNCE);
-    }
-  }
-
-
-
   //RETRIEVES LNG/LAT ON cLICK
   $("#ajaxButton").on("click", function (event) {
     event.preventDefault();
 
+    const address = $('#formatted-address').text();
+
     $.ajax({
       url: "/create",
       method: "POST",
-      data: { latitude: $('.latitudeBox').val(), longitude: $('.longitudeBox').val() },
+      data: { address: address, latitude: $('.latitudeBox').val(), longitude: $('.longitudeBox').val() },
 
       success: function (data) {
         console.log("SUCCESS WE DID THE AJAX CALL ON CLIENT'S END")
@@ -123,111 +160,5 @@ $(document).ready(function () {
     })
   });
 
-
   initMap();
 });
-
-
-
-
-
-// custom icons and labels
-// to the base of the flagpole.
-
-$(document).ready(function () {
-
-  // function initMap() {
-  //   const map = new google.maps.Map(document.getElementById("map"), {
-  //     zoom: 10,
-  //     center: { lat: -33.9, lng: 151.2 },
-  //   });
-
-  //   setMarkers(map);
-  // }
-
-  // // Data for the markers consisting of a name, a LatLng and a zIndex for the
-  // // order in which these markers should display on top of each other.
-  // const beaches = [
-  //   ["Bondi Beach", -33.890542, 151.274856, 4],
-  //   ["Coogee Beach", -33.923036, 151.259052, 5],
-  //   ["Cronulla Beach", -34.028249, 151.157507, 3],
-  //   ["Manly Beach", -33.80010128657071, 151.28747820854187, 2],
-  //   ["Maroubra Beach", -33.950198, 151.259302, 1],
-  // ];
-
-  // function setMarkers(map) {
-  //   // Adds markers to the map.
-  //   // Marker sizes are expressed as a Size of X,Y where the origin of the image
-  //   // (0,0) is located in the top left of the image.
-  //   // Origins, anchor positions and coordinates of the marker increase in the X
-  //   // direction to the right and in the Y direction down.
-  //   const image = {
-  //     url: "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png",
-  //     // This marker is 20 pixels wide by 32 pixels high.
-  //     size: new google.maps.Size(20, 32),
-  //     // The origin for this image is (0, 0).
-  //     origin: new google.maps.Point(0, 0),
-  //     // The anchor for this image is the base of the flagpole at (0, 32).
-  //     anchor: new google.maps.Point(0, 32),
-  //   };
-  //   // Shapes define the clickable region of the icon. The type defines an HTML
-  //   // <area> element 'poly' which traces out a polygon as a series of X,Y points.
-  //   // The final coordinate closes the poly by connecting to the first coordinate.
-  //   const shape = {
-  //     coords: [1, 1, 1, 20, 18, 20, 18, 1],
-  //     type: "poly",
-  //   };
-
-  //   for (let i = 0; i < beaches.length; i++) {
-  //     const beach = beaches[i];
-
-  //     new google.maps.Marker({
-  //       position: { lat: beach[1], lng: beach[2] },
-  //       map,
-  //       icon: image,
-  //       shape: shape,
-  //       title: beach[0],
-  //       zIndex: beach[3],
-  //     });
-  //   }
-  // }
-
-});
-
-
-
-
-
-/// ------------> BELOW DOESNT PLACE A MARKER.
-///  -----------> lng and lat from event listener "onclick"
-
-// function initMap() {
-//   const map = new google.maps.Map(document.getElementById("map"), {
-//     zoom: 12.5,
-//     center: { lat: 49.246292, lng: -123.116226 },
-//   });
-//   map.addListener("click", (event) => {
-//     // console.log("LAT--->", event.latLng.lat());
-//     $('.latitudeBox').val(event.latLng.lat());
-
-//     // console.log("LNG--->", event.latLng.lng());
-//     $('.longitudeBox').val(event.latLng.lng());
-//   })
-
-//   marker = new google.maps.Marker({
-//     map,
-//     draggable: true,
-//     animation: google.maps.Animation.DROP,
-//     position: { lat: 49.251754722903854, lng: -122.97958354394531 },
-
-//     //marker location
-//   });
-//   marker = new google.maps.Marker({
-//     map,
-//     draggable: true,
-//     animation: google.maps.Animation.DROP,
-//     position: { lat: 49.273321738192486, lng: -123.24619670100792 },
-//   })
-
-//   marker.addListener("click", toggleBounce);
-// }
