5998990343dd25b08bd72ffd17e6a617f612eaba
kenny-tse <kennytse91@gmail.com>
2021-10-21 07:24:20 +0000

    successful full circle of data

--- public/scripts/exploreId.js
+++ public/scripts/exploreId.js
@@ -2,24 +2,51 @@
 
 $(document).ready(function () {
 
-  function initMap(obj) {
+  function initMap() {
     const vancouver = { lat: 49.246292, lng: -123.116226 };
     const map = new google.maps.Map(document.getElementById("map"), {
       zoom: 12,
       center: vancouver,
     });
-    retrieveMarkers(db);
   }
 
-  function retrieveMarkers(db) {
-    for (let x = 0; x < db.length; x++) {
+  function retrieveMarkers(db, map) {
+    console.log("IM INSIDE RERTIEVE FUNCTION")
+    for (let i = 0; i < db.length; i++) {
+      console.log("THIS IS MY INDEX VALUE", db[i])
       new google.maps.Marker({
-        position: { lat: db[i].latitude, lng: db[i].longitude },
-        map: map,
+        position: { lat: Number(db[i].latitude), lng: Number(db[i].longitude) },
+        map,
         title: db[i].title,
+        icon: '/IMGS/marker-small.png',
       })
     }
   };
+
+  $("#buttonPressForStuff").on("click", (event) => {
+
+    $.ajax({
+      url: "/explore/afterLoad/1",
+      method: "GET",
+      data: { id: 1 },
+      success: function (data) {
+
+        function initMap() {
+          const vancouver = { lat: 49.246292, lng: -123.116226 };
+          const map = new google.maps.Map(document.getElementById("map"), {
+            zoom: 10,
+            center: vancouver,
+          });
+          retrieveMarkers(data.markers, map);
+        }
+        initMap();
+      },
+      error: function (error) {
+        console.log(error)
+      }
+    })
+
+  })
 
   initMap();
 
