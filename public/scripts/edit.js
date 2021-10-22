d980d798d42087ef2030fe3782bf1d53ed8fc126
Cindy H <cindyhaa@gmail.com>
2021-10-22 08:39:42 +0000

    edit

--- public/scripts/edit.js
+++ public/scripts/edit.js
@@ -2,4 +2,8 @@ $(document).ready(function () {
   $("#addNewMarker").on("click", function () {
     $("#newMarkerWrap").slideDown('swing');
   });
+
+  $("#map").on("mousedown", function () {
+    $(".content-large").slideDown('swing');
+  });
 });
