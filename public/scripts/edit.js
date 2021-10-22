064fd11e2cc0c15cefdd0ff0860a6575b9296aba
Cindy H <cindyhaa@gmail.com>
2021-10-22 07:57:24 +0000

    edit query

--- public/scripts/edit.js
+++ public/scripts/edit.js
@@ -1,19 +1,4 @@
 $(document).ready(function () {
-  $(function () {
-    $(window).scroll(function () {
-      if ($(this).scrollTop() - 200 > 0) {
-        $('#to-top').stop().slideDown('fast');
-      } else {
-        $('#to-top').stop().slideUp('fast');
-      }
-    });
-  });
-  $("#to-top").on("click", function () {
-    $("html, body").animate({
-      scrollTop: 0
-    }, 200);
-  });
-
   $("#addNewMarker").on("click", function () {
     $("#newMarkerWrap").slideDown('swing');
   });
